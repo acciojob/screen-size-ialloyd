6a6618491e1303d39140d9d57c34dfedac4ab6d2
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-20 19:59:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 //your JS code here. If required.
-window.addEventListener('resize', ()=>{
-	let width = window.innerWidth;
+window.addEventListener('resize', displayWindowSize);
+
+
+function displayWindowSize() {
+    let width = window.innerWidth;
     let height = window.innerHeight;
     let sizeInfo = document.getElementById('sizeInfo');
     sizeInfo.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
-});
+}
+
+displayWindowSize();
 
 
 
